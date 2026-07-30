e9f82c213376e9c1ae15be1c8cd883993716e739
Vanshika <vanshika.connects@gmail.com>
2026-07-30 22:12:06 +0530

    feat: add task templates and formatting renderer (Issue 11)

--- src/slmforge/task/__init__.py
+++ src/slmforge/task/__init__.py
@@ -7,6 +7,17 @@ from .detector import (
     CHAT,
     ALL_TASK_TYPES,
 )
+from .templates import (
+    TaskTemplate,
+    ClassificationTemplate,
+    SummarisationTemplate,
+    QATemplate,
+    InstructionTemplate,
+    ChatTemplate,
+    get_template,
+    render_record,
+    strip_record,
+)
 
 __all__ = [
     "detect_task",
@@ -16,4 +27,13 @@ __all__ = [
     "INSTRUCTION",
     "CHAT",
     "ALL_TASK_TYPES",
+    "TaskTemplate",
+    "ClassificationTemplate",
+    "SummarisationTemplate",
+    "QATemplate",
+    "InstructionTemplate",
+    "ChatTemplate",
+    "get_template",
+    "render_record",
+    "strip_record",
 ]
